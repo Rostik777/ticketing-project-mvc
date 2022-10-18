d2523b43eedbd60424521b9b7ed1d5782c45c0ed
rostyslavmoskal <moskalrostyslav@gmail.com>
2022-10-18 13:10:52 -0700

    feat: project service added

---
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,47 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Component
+public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String> implements ProjectService {
+    @Override
+    public ProjectDTO save(ProjectDTO object) {
+        if(object.getProjectStatus() == null) {
+            object.setProjectStatus(Status.OPEN);
+        }
+        return super.save(object.getProjectCode(), object);
+    }
+
+    @Override
+    public List<ProjectDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void update(ProjectDTO object) {
+        if(object.getProjectStatus() == null) {
+            object.setProjectStatus(findById(object.getProjectCode()).getProjectStatus());
+        }
+        super.update(object.getProjectCode(), object);
+    }
+
+    @Override
+    public void deleteById(String id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public ProjectDTO findById(String id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void complete(ProjectDTO project) {
+        project.setProjectStatus(Status.COMPLETE);
+        super.save(project.getProjectCode(), project);
+    }
+}
